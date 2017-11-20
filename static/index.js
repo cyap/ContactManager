ddd906f9385ffb54ffeb83765374d5874a6b0f36
Chester Yap <chesterdyap@gmail.com>
2017-11-19 22:37:40 -0600

    Implement front-end date validation

--- static/index.js
+++ static/index.js
@@ -1,10 +1,27 @@
 window.onload = function() {
+
+	// Attach date validator to each day in dropdown
+	$('#id_birthdate_1 > option').attr('v-if', function(i) {
+		return 'this.validDay(' + (i+1) + ')'
+	})
+	
 	new Vue({
 		el: '#contact-submission', 
 		data: {
 			contact: { 
-				last_name: 'Test' 
-			}
+				birthdate: {
+				},
+				last_name: 'Test',
+				address: 'ad'
+			},
+		},
+		methods: {
+			validDay: function(day) {
+				// Check if each day can exist in selected month
+				if(day > new Date(2020, this.contact.birthdate.month, 0).getDate())
+					return false;
+				return true;
+			}	
 		},
 		delimiters: ['[[', ']]']
 	});
